ed766d63476b38c4145cc2c68726943acec866ef
Asmaa Gamal <asmaagamal.nagy@gmail.com>
2025-02-26 20:49:18 +0200

    third edition

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule ,FormControl } from '@angular/forms';
 import { FormField } from '../../models/form-field.model';
 import { FormService } from '../../services/form.service';
 import { CommonModule } from '@angular/common';
@@ -39,6 +39,7 @@ export class DynamicFormComponent implements OnInit {
   constructor(private fb: FormBuilder, private formService: FormService) {}
 
   ngOnInit() {
+
     this.formConfig = this.formService.getFormConfig();
     this.buildForm();
   }
@@ -46,7 +47,42 @@ export class DynamicFormComponent implements OnInit {
   buildForm() {
     this.form = this.fb.group({});
 
-    this.formConfig.forEach(field => this.addField(field, this.form));
+    // this.formConfig.forEach(field => {
+    //   this.addField(field, this.form);
+    // });
+    this.formConfig.forEach(field => {
+      if (field.type === 'group' && field.children) {
+        const nestedGroup = this.fb.group({});
+        field.children.forEach(child => this.addField(child, nestedGroup));
+        this.form.addControl(field.name, nestedGroup);
+      } else {
+        this.form.addControl(field.name, new FormControl('', this.getValidators(field.validations)));
+      }
+    });
+  }
+
+  addField(field: FormField, group: FormGroup) {
+    if (field.type === 'group' && field.children) {
+      const nestedGroup = this.fb.group({});
+      field.children.forEach(child => this.addField(child, nestedGroup));
+      group.addControl(field.name, nestedGroup);
+    } else {
+      const control = this.fb.control(
+        field.value || '',  // Default value
+        this.getValidators(field.validations) // Apply validators
+      );
+      group.addControl(field.name, control);
+    }
+  }
+
+
+  getValidators(validations?: any) {
+    const validatorList = [];
+    if (validations?.required) validatorList.push(Validators.required);
+    if (validations?.minLength) validatorList.push(Validators.minLength(validations.minLength));
+    if (validations?.maxLength) validatorList.push(Validators.maxLength(validations.maxLength));
+    if (validations?.pattern) validatorList.push(Validators.pattern(validations.pattern));
+    return validatorList;
   }
   getErrorMessage(field: string): string {
     const control = this.form.get(field);
@@ -61,28 +97,6 @@ export class DynamicFormComponent implements OnInit {
   isEmpty(obj: any): boolean {
     return obj && Object.keys(obj).length === 0;
   }
-
-
-  addField(field: FormField, group: FormGroup) {
-    if (field.type === 'group' && field.children) {
-      const nestedGroup = this.fb.group({});
-      field.children.forEach(child => this.addField(child, nestedGroup));
-      group.addControl(field.name, nestedGroup);
-    } else {
-      const control = this.fb.control(field.value || '', this.getValidators(field.validations));
-      group.addControl(field.name, control);
-    }
-  }
-
-  getValidators(validations?: any) {
-    const validatorList = [];
-    if (validations?.required) validatorList.push(Validators.required);
-    if (validations?.minLength) validatorList.push(Validators.minLength(validations.minLength));
-    if (validations?.maxLength) validatorList.push(Validators.maxLength(validations.maxLength));
-    if (validations?.pattern) validatorList.push(Validators.pattern(validations.pattern));
-    return validatorList;
-  }
-
   onSubmit() {
     if (this.form.valid) {
       alert('Form submitted successfully!');
